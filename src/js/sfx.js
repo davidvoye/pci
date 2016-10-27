4611fe3edb00b6b7c6d5c3d70e53a62e82e8c192
david voye <david.voye@wwu.edu>
2016-10-27 15:00:19 -0700

    Changed sfx.js

--- src/js/sfx.js
+++ src/js/sfx.js
@@ -1,15 +1,4 @@
 (function ($, Drupal, window, document, undefined) {
 
-  Drupal.behaviors.mainMenuOffset = {
-
-    attach: function (context) {
-      var $main_menu = $('#main-menu', context);
-
-      $main_menu.css({
-        bottom: -($main_menu.height() / 2),
-      });
-    }
-
-  };
 
 }) (jQuery, Drupal, this, this.document);
